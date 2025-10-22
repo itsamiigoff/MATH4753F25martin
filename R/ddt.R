e1da7349a91feaf44e8a9efa0cec8c15ab4405f8
itsamiigoff <itsamiigoff@gmail.com>
2025-10-22 14:39:46 -0500

    Add ddt data (practice)

---
+++ R/ddt.R
@@ -0,0 +1,14 @@
+#' ddt data
+#'
+#' A subset of ddt data from Australia
+#'
+#' @format ## `who`
+#' A data frame with 7,240 rows and 60 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"ddt"
