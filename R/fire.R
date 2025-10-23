024dfdf9abe4dd0cfc0ead555bb5bb9984ed2bd7
itsamiigoff <itsamiigoff@gmail.com>
2025-10-23 18:58:27 -0500

    Add fire damage data

---
+++ R/fire.R
@@ -0,0 +1,12 @@
+#' fire data
+#'
+#' A dataset collected concerning home damage from fire
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'   \item{DISTANCE}{distance from fire station in miles}
+#'   \item{DAMAGE}{x1000 dollars in damage}
+#' }
+#'
+#' @source From textbook
+"fire"
